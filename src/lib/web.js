d7a195b2f6b617b78110d3aba5b5ffc0f053f397
shaman2016scratch <g.ilkh@yandex.ru>
2026-09-04 08:47:36 +0300

    Upd

--- src/lib/web.js
+++ src/lib/web.js
@@ -1,6 +1,28 @@
 import { logs } from "./cli-help.js";
 import CLI from "../cli.js";
 
-class WebInput {}
+class WebInput {
+    constructor (domElement) {
+        this.input = domElement
+    }
 
-coCLI("~", "User1234", logs, {}, { createInterface: () => { return new WebInput() } })
+    async question (text) {
+        const id = crypto.randomBytes(Math.floor(Math.random() * (64 - 32 + 1) + 32)).toString()
+        window.isInputted = false
+        this.input.innerHTML += `${text}<input type="text" id="input-id${id}"><button onclick="window.isInputted = true"></button><br>`
+        const generator = () => await new Promise(resolve => {
+            if (!window.isInputted) {
+                // ignore
+            } else {
+                resolve(true)
+            }
+        })
+        return document.getElementById(`input-id${id}`).value
+    }
+
+    close () {}
+}
+
+const WebVirtualCLI = (input) => {
+    CLI("~", "User1234", logs, {}, { createInterface: () => { return new WebInput(input) } })
+}
